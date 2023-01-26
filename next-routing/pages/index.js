c1565e78d20338f595e94e4e9b510f0409d4b152
Haritha Reddy Peddireddy <99361160+haritha121@users.noreply.github.com>
2023-01-26 12:44:32 -0600

    Implemented component navigation in Home component, Product Component  by importing Link Component.

--- next-routing/pages/index.js
+++ next-routing/pages/index.js
@@ -1,4 +1,16 @@
+import Link from "next/link";
+
 function Home() {
-  return <h1>Home Page</h1>;
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <Link href="/blog" legacyBehavior>
+        <a>Blog </a>
+      </Link>
+      <Link href="/product" legacyBehavior>
+        <a> Product</a>
+      </Link>
+    </div>
+  );
 }
 export default Home;
--- next-routing/pages/product/index.js
+++ next-routing/pages/product/index.js
@@ -1,9 +1,30 @@
-function ProductList() {
+import Link from "next/link";
+function ProductList({ productID = 100 }) {
   return (
     <>
-      <h2>Product1</h2>
-      <h2>Product2</h2>
-      <h2>Product3</h2>
+      <Link href="/" legacyBehavior>
+        <a>Home</a>
+      </Link>
+      <h2>
+        <Link href="/product/1" legacyBehavior>
+          <a>Product 1</a>
+        </Link>
+      </h2>
+      <h2>
+        <Link href="/product/2" legacyBehavior>
+          <a>Product 2</a>
+        </Link>
+      </h2>
+      <h2>
+        <Link href="/product/3" legacyBehavior replace>
+          <a>Product 3</a>
+        </Link>
+      </h2>
+      <h2>
+        <Link href={`/product/${productID}`} legacyBehavior>
+          <a> Product {productID}</a>
+        </Link>
+      </h2>
     </>
   );
 }
