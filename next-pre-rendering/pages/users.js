0d79d48bd182f0d169d5bc10ee3e71deb4297d2f
Haritha Reddy Peddireddy <99361160+haritha121@users.noreply.github.com>
2023-01-27 17:13:50 -0600

    Implemented Static Generation with data using getStaticProps function in users.js file.

---
+++ next-pre-rendering/pages/users.js
@@ -0,0 +1,27 @@
+function Users({ users }) {
+  return (
+    <>
+      <h1>Users Data</h1>
+      {users.map((user) => {
+        return (
+          <div key={user.id}>
+            <p>{user.name}</p>
+            <p>{user.email}</p>
+          </div>
+        );
+      })}
+    </>
+  );
+}
+
+export default Users;
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await response.json();
+  console.log(data);
+  return {
+    props: {
+      users: data,
+    },
+  };
+}
