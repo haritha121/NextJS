676b34b698b78cbc32c89dc112d363c5b700df14
Haritha Reddy Peddireddy <99361160+haritha121@users.noreply.github.com>
2023-02-28 14:59:34 -0600

    Fixed build issue

--- next-pre-rendering/pages/posts/[postId].js
+++ next-pre-rendering/pages/posts/[postId].js
@@ -1,4 +1,12 @@
+import { useRouter } from "next/router";
+
 function Post({ post }) {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <>
       <h2>
@@ -10,7 +18,35 @@ function Post({ post }) {
 }
 
 export default Post;
+export async function getStaticProps(context) {
+  const { params } = context; //to get hold of params in the url getstaticprops will have context keyword from which we can extract the required parameter entered in the url.
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${params.postId}`
+  );
+  const data = await response.json();
+
+  if (!data.id) {
+    return {
+      notFound: true,
+    };
+  }
+
+  console.log(`Generating page for /posts/${params.postId}`);
+  return {
+    props: {
+      post: data,
+    },
+  };
+}
+
 export async function getStaticPaths() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await response.json();
+  const paths = data.map((post) => {
+    return {
+      params: { postId: `${post.id}` }, //dynamic way of rendering path
+    };
+  });
   return {
     paths: [
       { params: { postId: "1" } }, //here we informed next js to generate the postId with postId=1;
@@ -18,17 +54,5 @@ export async function getStaticPaths() {
       { params: { postId: "3" } }, //we are only passing three posts so we are generating 3 paths
     ],
     fallback: true,
-  };
-}
-export async function getStaticProps(context) {
-  const { params } = context; //to get hold of params in the url getstaticprops will have context keyword from which we can extract the required parameter entered in the url.
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/posts/${params.postId}`
-  );
-  const data = await response.json();
-  return {
-    props: {
-      post: data,
-    },
   };
 }
--- next-pre-rendering/pages/posts/index.js
+++ next-pre-rendering/pages/posts/index.js
@@ -1,31 +1,36 @@
 import Link from "next/link";
+
 function PostList({ posts }) {
   return (
     <>
-      <h1> Lists of posts</h1>
+      <h1>List of Posts</h1>
       {posts.map((post) => {
         return (
           <div key={post.id}>
             <Link href={`posts/${post.id}`} passHref>
               {/* //We will use passHref in Link tag when the child for the Link tag is not an anchor tag */}
+
               <h2>
                 {post.id} {post.title}
               </h2>
             </Link>
+            <hr />
           </div>
         );
       })}
     </>
   );
 }
+
+export default PostList;
+
 export async function getStaticProps() {
   const response = await fetch("https://jsonplaceholder.typicode.com/posts");
   const data = await response.json();
+
   return {
     props: {
       posts: data.slice(0, 3),
     },
   };
 }
-
-export default PostList;
