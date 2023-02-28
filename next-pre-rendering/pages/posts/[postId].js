8ff1a708b5b51315c3d95a4e45423ff576fdcec9
Haritha Reddy Peddireddy <99361160+haritha121@users.noreply.github.com>
2023-02-28 14:26:08 -0600

    Fixing build issue

--- next-pre-rendering/pages/posts/[postId].js
+++ next-pre-rendering/pages/posts/[postId].js
@@ -10,12 +10,22 @@ function Post({ post }) {
 }
 
 export default Post;
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: { postId: "1" } }, //here we informed next js to generate the postId with postId=1;
+      { params: { postId: "2" } }, //postId paramter value is a string
+      { params: { postId: "3" } }, //we are only passing three posts so we are generating 3 paths
+    ],
+    fallback: true,
+  };
+}
 export async function getStaticProps(context) {
   const { params } = context; //to get hold of params in the url getstaticprops will have context keyword from which we can extract the required parameter entered in the url.
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/posts/${params.postId}`
   );
-  const data = response.json();
+  const data = await response.json();
   return {
     props: {
       post: data,
