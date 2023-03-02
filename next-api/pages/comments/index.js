125f5f3321499303f80642ff8910579cb23fe372
Haritha Reddy Peddireddy <99361160+haritha121@users.noreply.github.com>
2023-03-02 12:22:10 -0600

    Implemented post request on click of submitcomment in comments page

--- next-api/pages/comments/index.js
+++ next-api/pages/comments/index.js
@@ -2,13 +2,31 @@ import { useState } from "react";
 
 function CommentsPage() {
   const [comments, setComments] = useState([]);
+  const [userComment, setUserComment] = useState("");
   const fetchComments = async () => {
     const response = await fetch("/api/comments");
     const data = await response.json();
     setComments(data);
   };
+  const handleSubmitComment = async () => {
+    const response = await fetch("/api/comments", {
+      method: "POST",
+      body: JSON.stringify({ userComment }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    console.log("Response from POST reaquest", data);
+  };
   return (
     <>
+      <input
+        type="text"
+        value={userComment}
+        onChange={(e) => setUserComment(e.target.value)}
+      />
+      <button onClick={handleSubmitComment}>Submit Comment</button>
       <button onClick={fetchComments}>Load comments</button>
       {comments.map((comment) => {
         return (
