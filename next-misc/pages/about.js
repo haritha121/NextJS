82d8af5f8f832454b3a45879d4b2da4cd88041d5
Haritha Reddy Peddireddy <99361160+haritha121@users.noreply.github.com>
2023-03-06 12:55:01 -0600

    Implemented head component in about.js and dynamic head component in [blogId].js

--- next-misc/pages/about.js
+++ next-misc/pages/about.js
@@ -1,7 +1,19 @@
+import Head from "next/head";
 import Footer from "@/components/Footer";
 
 function About() {
-  return <div className="content">About</div>;
+  return (
+    <>
+      <Head>
+        <title>About Page Head Component Implementation</title>
+        <meta
+          name="description"
+          content="Learning next js from Code evolution"
+        />
+      </Head>
+      <h1 className="content">About</h1>
+    </>
+  );
 }
 
 export default About;
