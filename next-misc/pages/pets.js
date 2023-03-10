391390938060003892252741b463255633e01055
Haritha Reddy Peddireddy <99361160+haritha121@users.noreply.github.com>
2023-03-10 11:34:09 -0600

    Implemnted aboslute path imports in _app.js

--- next-misc/pages/pets.js
+++ next-misc/pages/pets.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import img from "../public/Image1.jpg";
+import img from "public/Image1.jpg";
 function PetsPage() {
   const shimmer = (w, h) => `
 <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
