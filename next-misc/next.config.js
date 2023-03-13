7c3d14b9d864c1674c3d4a5557acbd5b53cfdba5
Haritha Reddy Peddireddy <99361160+haritha121@users.noreply.github.com>
2023-03-13 13:17:19 -0500

    Implemented redirects

--- next-misc/next.config.js
+++ next-misc/next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  redirects: async () => {
+    return [
+      {
+        source: "/about",
+        destination: "/",
+        permanent: true, //indicates whether the redirection is permanent or not.
+      },
+      //path matching redirects
+      {
+        source: "/old-blog/:id",
+        destination: "/new-blog/:id",
+        permanent: true, //indicates whether the redirection is permanent or not.
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
