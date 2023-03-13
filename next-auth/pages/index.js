6dbf047277c566b624afc606b17fb091e870043d
Haritha Reddy Peddireddy <99361160+haritha121@users.noreply.github.com>
2023-03-13 18:13:04 -0500

    Added welocme message along with the authorized user name in Home page

--- next-auth/pages/index.js
+++ next-auth/pages/index.js
@@ -1,11 +1,13 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import { useSession } from "next-auth/react";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const { data: session, status } = useSession();
   return (
     <>
       <Head>
@@ -15,6 +17,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
+        <h1 className={styles.title}>
+          {session ? `${session.user.name},` : " "} Welcome to{"Next JS"}
+        </h1>
         <div className={styles.description}>
           <p>
             Get started by editing&nbsp;
@@ -26,7 +31,7 @@ export default function Home() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              By{' '}
+              By{" "}
               <Image
                 src="/vercel.svg"
                 alt="Vercel Logo"
@@ -119,5 +124,5 @@ export default function Home() {
         </div>
       </main>
     </>
-  )
+  );
 }
