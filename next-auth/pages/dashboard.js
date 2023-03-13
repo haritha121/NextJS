3d426a600523a5eea3510f45e5d04fc9c836ed5c
Haritha Reddy Peddireddy <99361160+haritha121@users.noreply.github.com>
2023-03-13 17:58:34 -0500

    Implemented how to secure pages client-side

--- next-auth/pages/dashboard.js
+++ next-auth/pages/dashboard.js
@@ -1,7 +1,34 @@
-import React from "react";
+// import { useSession, signIn } from "next-auth/react";
 
-function Dashboard() {
-  return <h1>Dashboard</h1>;
+// function Dashboard({}) {
+//   const { status } = useSession({
+//     required: true,
+//     onUnauthenticated() {
+//       // The user is not authenticated, handle it here.
+//       signIn();
+//     },
+//   });
+//   return <h1>Dashboard</h1>;
+// }
+
+// export default Dashboard;
+
+// Here is the beginner way, to protect pages for "Unauthenticated" user. next-auth Version 4 :
+
+import React, { useEffect } from "react";
+import { useSession, signIn } from "next-auth/react";
+export default function Dashboard() {
+  const { status } = useSession();
+  useEffect(() => {
+    if (status === "unauthenticated") signIn();
+  }, [status]);
+  if (status !== "authenticated") {
+    return <h2>Loading...</h2>;
+  }
+  return (
+    <>
+      {" "}
+      <div>Dashboard</div>{" "}
+    </>
+  );
 }
-
-export default Dashboard;
