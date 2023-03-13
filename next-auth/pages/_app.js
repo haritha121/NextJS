9b959ea17e26e552e7900294428a3fc3116af1dc
Haritha Reddy Peddireddy <99361160+haritha121@users.noreply.github.com>
2023-03-13 15:33:39 -0500

    Installed next-auth , configured and setup.

--- next-auth/pages/_app.js
+++ next-auth/pages/_app.js
@@ -1,5 +1,12 @@
-import '@/styles/globals.css'
+import Navbar from "@/components/Navbar";
+// import "@/styles/globals.css";
+import "../components/Navbar.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  );
 }
