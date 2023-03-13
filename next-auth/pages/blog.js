92c797b2435f3a62555d45e7fc96e36b0d7e646e
Haritha Reddy Peddireddy <99361160+haritha121@users.noreply.github.com>
2023-03-13 18:32:52 -0500

    Implemented serverside authentication getServerSideProps in blog page

--- next-auth/pages/blog.js
+++ next-auth/pages/blog.js
@@ -1,5 +1,20 @@
-function Blog() {
-  return <h1>Blog</h1>;
+import { getSession, useSession } from "next-auth/react";
+function Blog({ blogsdata }) {
+  const { data: session } = useSession(); //if we use session like this then if session is sent as props useSession will make use of session from props else it will use the session from it.
+  console.log(session);
+  return <h1>Blog {blogsdata}</h1>;
 }
 
 export default Blog;
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+  return {
+    props: {
+      session,
+      blogsdata: session
+        ? "List of 100 personalizedblogs"
+        : "List of free blogs",
+    },
+  };
+}
